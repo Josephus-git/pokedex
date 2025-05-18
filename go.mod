c6932ab9601f2d9ba8f78779f6e9049cd509782c
josephus <otejerejosephus@gmail.com>
2025-05-18 13:30:49 +0100

    got the read from input working correctly

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/josephus-git/pokedex
+
+go 1.22.2
---
+++ repl_test.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestCleanInput(t *testing.T) {
+	cases := []struct {
+		input    string
+		expected []string
+	}{
+		{
+			input:    " hello world ",
+			expected: []string{"hello", "world"},
+		},
+	}
+
+	for _, c := range cases {
+		actual := cleanInput(c.input)
+		if len(actual) != len(c.expected) {
+			t.Errorf("length not equal, Expected, Actual-> %d, %d", len(c.expected), len(actual))
+			return
+		}
+		for i := range actual {
+			word := actual[i]
+			expectedWord := c.expected[i]
+			if word != expectedWord {
+				t.Errorf("word does not match")
+				return
+			}
+		}
+	}
+}
